08c7ab39dbee789ef0519aceccc7c1832499061d
dang78dz <100210394+dang78dz@users.noreply.github.com>
2025-09-18 22:02:45 +0700

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,35 @@
-// Import Firebase scripts
-importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging.js");
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-// Firebase config (giống index.html)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBw-z3LcKNCQLrzJeNsPf73SrCjemjgSTE",
-  authDomain: "d9-902ab.firebaseapp.com",
-  projectId: "d9-902ab",
-  storageBucket: "d9-902ab.firebasestorage.app",
-  messagingSenderId: "313463134110",
-  appId: "1:313463134110:web:a2d79097cad2ccdfc3288f",
-  measurementId: "G-G1PVZHR1MD"
+    apiKey: "AIzaSyBw-z3LcKNCQLrzJeNsPf73SrCjemjgSTE",
+    authDomain: "d9-902ab.firebaseapp.com",
+    projectId: "d9-902ab",
+    storageBucket: "d9-902ab.firebasestorage.app",
+    messagingSenderId: "313463134110",
+    appId: "1:313463134110:web:a2d79097cad2ccdfc3288f",
+    measurementId: "G-G1PVZHR1MD"
 };
 
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-// Xử lý khi nhận thông báo ở background
 messaging.onBackgroundMessage((payload) => {
-  console.log("Thông báo background:", payload);
-  self.registration.showNotification(payload.notification.title, {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: "/icon.png" // có thể thay icon của lớp
-  });
+    icon: '/firebase-logo.png' // Optional: a logo for the notification
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
